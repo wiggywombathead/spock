ad147e9383a7077f495b57a1124615c2439fd232
Tom Archbold <wiggywombathead@hotmail.co.uk>
2019-09-26 18:06:34 +0100

    Fixed linking in Makefile

--- src/main.cpp
+++ src/main.cpp
@@ -238,12 +238,12 @@ VkInstance createInstance(
 	VkInstance instance;
 
 	VkApplicationInfo appInfo = {};
-	appInfo.sType                 = VK_STRUCTURE_TYPE_APPLICATION_INFO;
-	appInfo.pApplicationName      = name;
-	appInfo.applicationVersion    = VK_MAKE_VERSION(1, 0, 0);
-	appInfo.pEngineName			  = "spock";
-	appInfo.engineVersion         = VK_MAKE_VERSION(1, 0, 0);
-	appInfo.apiVersion            = VK_API_VERSION_1_0;
+	appInfo.sType              = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+	appInfo.pApplicationName   = name;
+	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.pEngineName        = "spock";
+	appInfo.engineVersion      = VK_MAKE_VERSION(1, 0, 0);
+	appInfo.apiVersion         = VK_API_VERSION_1_0;
 
 	VkInstanceCreateInfo instanceCI = {};
 	instanceCI.sType                   = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
